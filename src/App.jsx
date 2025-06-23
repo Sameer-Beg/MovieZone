8008a90d38867fc13ce23d647122d86765f495b2
Sameer Beg <sameer423j@gmail.com>
2025-06-23 11:30:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Movies from './MovieZone/Movies'
+
+const App = () => {
+  return (
+    <div>
+      <>
+      <div>
+        <Movies/>
+      </div>
+      </>
+    </div>
+  )
+}
+
+export default App
